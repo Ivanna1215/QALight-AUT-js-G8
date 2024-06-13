e08cc7ec4bf35c4b9a39b3320ea9b5fdf93c4f34
Kovaliv Ivanna <ivanna.kovaliv1215@gmail.com>
2024-06-13 13:21:37 +0300

    Updated homework 4

--- cypress/e2e/homework_4.cy.js
+++ cypress/e2e/homework_4.cy.js
@@ -3,12 +3,14 @@ describe('Homework 4', () => {
     const testCases = [
         {
             testData: [
-                { position: 'top-right', title: 'water', content: 'primary', time: '10000', type: 'primary' },
-                { position: 'top-left', title: 'eggs', content: 'success', time: '20000', type: 'success' }
+                { position: 'top-right', title: 'Ivanna', content: 'primary', time: '10000', type: 'primary' },
+                { position: 'top-left', title: 'Anna', content: 'success', time: '20000', type: 'success' },
+                { position: 'bottom-left', title: 'Nata', content: 'info', time: '30000', type: 'info' },
             ],
             expectedResult: [
-                { icon: 'email', title: 'water', content: 'primary', color: 'rgb(98, 0, 238)', position: 'top-right' },
-                { icon: 'email', title: 'eggs', content: 'success', color: 'rgb(56, 142, 60)', position: 'top-left' }
+                { icon: 'email', title: 'Ivanna', content: 'primary', color: 'rgb(98, 0, 238)',  position: 'cdk-overlay-0' },
+                { icon: 'checkmark', title: 'Anna', content: 'success', color: 'rgb(96, 175, 32)', position: 'cdk-overlay-2' },
+                { icon: 'question-mark', title: 'Nata', content: 'info', color: 'rgb(4, 149, 238)',  position: 'cdk-overlay-5' }
             ]
         }
     ];
@@ -18,29 +20,29 @@ describe('Homework 4', () => {
             const { position, title, content, time, type } = data;
             const expected = expectedResult[index];
 
+            it(`Enter "${title}" `, () => {
+                cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com/');
+                cy.contains('Material Light').click();
+                cy.contains('Modal & Overlays').click();
+                cy.get('[ng-reflect-router-link="/pages/modal-overlays/toastr"]').click();
 
-        it(`Enter "${title}" `, () => {
-            cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com/');
-            cy.contains('Material Light').click();
-            cy.contains('Modal & Overlays').click();
-            cy.get('[ng-reflect-router-link="/pages/modal-overlays/toastr"]').click();
+                cy.get('[class*="mat-ripple position-select "]').click();
+                cy.contains(position).click();
+                cy.get('[name="title"]').clear().type(title);
+                cy.get('[name="content"]').clear().type(content);
+                cy.get('[name="timeout"]').clear().type(time);
+                cy.get('[class*="mat-ripple appearance-outline"]').click();
+                cy.get('ul[class="option-list"]', { timeout: 5000 }).contains(type).click();
+                cy.contains('button', 'Show toast').click();
 
-            cy.get('[class*="mat-ripple position-select "]').click();
-            cy.contains(position).click();
-            cy.get('[name="title"]').clear().type(title);
-            cy.get('[name="content"]').clear().type(content);
-            cy.get('[name="timeout"]').clear().type(time);
-            cy.get('[class*="mat-ripple appearance-outline"]').click();
-            cy.get('ul[class="option-list"]', { timeout: 5000 }).contains(type).click();
-            cy.contains('button', 'Show toast').click();
+                cy.get(`[data-name=${expected.icon}]`).should('be.visible')
+                cy.get('[class="title subtitle"]').should('contain', expected.title)
+                cy.get('nb-toastr-container [class="message"]').should('contain', expected.content)
+                cy.get('[class*="ng-trigger ng-trigger-fadeIn"]').should('have.css','background-color', expected.color)
+                cy.get(`[class*="toastr-overlay-container"] [id=${expected.position}]`,{timeout:5000}).should('be.visible')
 
-
-            cy.get('[class="title subtitle"]').should('contain', expected.title)
-            cy.get('nb-toastr-container [class="message"]').should('contain',expected.content)
-
-
+            });
         });
     });
-});
 
 });
